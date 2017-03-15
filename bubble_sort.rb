c2023594e1c4020d16c0147e7458e3843a4ecb38
rongxanh88 <rongxanh88@gmail.com>
2017-03-15 16:32:59 -0600

    second version of bubble sort complete

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,55 +1,35 @@
 require 'pry'
 
 class BubbleSort
-  def sort(array)
-    @array = array
-    if @array.length < 2
+  def sort(collection)
+    if collection.length < 2
       puts "Array has less than two elements"
-      return
+      return collection
     end
-    
+
     sorted = false
-    first_pointer = 0
-    second_pointer = 1
-    
+    swapped = false
     while !sorted do
-      swaps = 0
-      current_element = @array[first_pointer]
-      next_element = @array[second_pointer]
-
-      if current_element > next_element
-        swap(first_pointer, second_pointer)
-        swaps += 1
-      end
-
-      first_pointer += 1
-      second_pointer += 1
-      
-      if second_pointer == (@array.length) #second pointer reaches end of array
-        first_pointer = 0
-        second_pointer = first_pointer + 1
-        
-        if swaps == 0
-          sorted = true
-        else
-          swaps = 0 #reset swaps counter
+      (collection.length - 1).times do |iteration|
+        #puts "#{collection}"
+        index = iteration
+        if collection[index] > collection[index + 1]
+          temp = collection[index + 1]
+          collection[index +1] = collection[index]
+          collection[index] = temp
+          swapped = true
         end
       end
 
+      if swapped == false #no swaps, finish bubble sort
+        sorted = true
+      end
+      swapped = false #reset swapped flag
     end
-    return @array
+    return collection
   end
-
-  def swap(index1, index2)
-    temp = @array[index1]
-    @array[index1] = @array[index2]
-    @array[index2] = temp
-  end
-  
-  
 end
 
-
 sorter = BubbleSort.new # need instance method
-sorted_array = sorter.sort(["d", "b", "a", "c"])
+sorted_array = sorter.sort(%w(e t k j k q a o l p))
 puts sorted_array
